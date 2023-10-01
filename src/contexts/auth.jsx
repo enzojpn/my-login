7f9ebe15a319b0350990912feb30410dfe23fa78
enzojpn <enzo_jpn@hotmail.com>
2023-10-01 09:54:37 -0300

    adicionando axios e consumindo api de auth com uso de token

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -2,6 +2,8 @@ import React, { createContext, useState, useEffect } from "react";
 
 import { useNavigate } from "react-router-dom";
 
+import { api, createSession } from "../services/api";
+
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
@@ -18,27 +20,32 @@ export const AuthProvider = ({ children }) => {
     }, [])
 
 
-    const login = (email, password) => {
-        console.log("login ", { email, password });
+    const login = async (email, password) => {
+
+        const response = await createSession(email, password)
+        console.log("login ", response.data);
 
         //api criar uma session
 
-        const loggedUser = {
-            id: "123",
-            email
-        }
+        //const loggedUser = { id: "123", email }
 
-        
-        if (password === "secret") {
-            localStorage.setItem("user", JSON.stringify(loggedUser));
-            setUser(loggedUser);
-            navigate("/")
-        }
+        const loggedUser = response.data.user;
+        const token = response.data.token;
+        localStorage.setItem("user", JSON.stringify(loggedUser));
+        localStorage.setItem("token", token);
+
+        api.defaults.headers.Authorization = `Bearer ${token}`
+
+        setUser(loggedUser);
+        navigate("/")
+
 
     }
     const logout = () => {
         console.log('loghout');
         localStorage.removeItem('user');
+        localStorage.removeItem('token');
+        api.defaults.headers.Authorization= null;
         setUser(null);
         navigate("/login");
     }
--- src/pages/HomePages/index.jsx
+++ src/pages/HomePages/index.jsx
@@ -1,20 +1,47 @@
-import React, { useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { AuthContext } from "../../contexts/auth";
+import { getUsers } from "../../services/api";
+
+
 
 const HomePage = () => {
+    const {  authenticate } = useContext(AuthContext);
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-const {logout , authenticate} = useContext(AuthContext);
-const handleLogout = () => {
-    logout();
-}
+    useEffect(() => {
+        (async () => {
+            const response = await getUsers();
+            setUsers(response.data);
+            setLoading(false);
+        })()
+    }, [])
 
-    return(
+    const handleLogout = () => {
+        logout();
+    }
+
+    if(loading){
+       return <div className="loading">Carregando dados....</div>
+    }
+
+    return (
         <>
-        <p>{String(authenticate)}</p>
-        <button onClick={handleLogout}>Logout</button>
-        <h1>HomePage</h1>
+            <p>{String(authenticate)}</p>
+            <button onClick={handleLogout}>Logout</button>
+            <ul>
+                {
+                    users.map((user)=>(
+                        <li key={id}>
+                            {user.id} - {user.email}
+                        </li>
+                    ))
+                    
+                }
+            </ul>
+            <h1>HomePage</h1>
         </>
-    ) 
+    )
 }
 
 export default HomePage 
